6e6217d7ec0d9bf047c85780db4f47a196ade4ca
steven rosa <scrosa13@outlook.com>
2022-09-20 12:52:08 -0400

    updating js and html

--- assets/script.js
+++ assets/script.js
@@ -1,32 +1,37 @@
-var button = document.querySelector('.button')
-var searchValue = document.querySelector('.searchValue')
-var cityName = document.querySelector('.cityName')
-var desc = document.querySelector('.desc')
-var temp = document.querySelector('.temp')
+var button = document.querySelector(".button");
+var searchValue = document.querySelector(".searchValue");
+var cityName = document.querySelector(".cityName");
+var currentDesc = document.getElementById("currentDesc");
+var currentTemp = document.getElementById("currentTemp");
+var currentWind = document.getElementById("currentWind");
+var currentHum = document.getElementById("currentHum");
 
-var aPIKey = "5043541d08d2d01a2a0ae76e4ef8627d"
-
-
-
+var aPIKey = "5043541d08d2d01a2a0ae76e4ef8627d";
 
 // https://api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
+// https://api.openweathermap.org/data/2.5/forecast?lat=57&lon=-2.15&appid=
 
+button.addEventListener("click", searchWeather);
 
-button.addEventListener('click', searchWeather)
-
-
-function searchWeather () {
+function searchWeather() {
   searchCity = searchValue.value;
-  fetch(`https://api.openweathermap.org/data/2.5/weather?q=` +searchCity+ `&appid=${aPIKey}`)
-  .then(response => response.json())
-  .then(data => console.log(data),
-    // .catch(err => alert("wrong city name!"))  
-  // cityName.innerHTML = data.name.value
-
-
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?q=` +
+      searchCity +
+      `&appid=${aPIKey}` +
+      `&units=imperial`
   )
+    .then((response) => response.json())
+    .then(function (data) {
+      console.log(data),
+
+        (currentTemp.textContent = "Temp" + '   ' + data.main.temp );
+        (currentWind.textContent = "Wind" + '   ' + data.wind.speed );
+        (currentHum.textContent = "Humidity" + '   ' + data.main.humidity );
+
+
+
+
+    });
 }
-
-  
-
