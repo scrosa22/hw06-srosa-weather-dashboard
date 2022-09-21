81b68429c2139950e1b9fade8685a61db6aa8d13
steven rosa <scrosa13@outlook.com>
2022-09-21 13:05:20 -0400

    updates

--- assets/script.js
+++ assets/script.js
@@ -9,13 +9,11 @@ var currentHum = document.getElementById("currentHum");
 var currentUV = document.getElementById("currentUV");
 var displaystoredcities = document.getElementById("searchHistory");
 var aPIKey = "5043541d08d2d01a2a0ae76e4ef8627d";
-var weeklytemp = document.getElementById('weekly')
-
+var weeklytemp = document.getElementById("weekly");
 
 function displayWeather() {
   city = searchValue.value;
   searchWeather(city);
-  // searchUV(city);
 }
 
 const searchWeather = () => {
@@ -35,17 +33,7 @@ const searchWeather = () => {
       currentHum.textContent = "Humidity" + "   " + data.main.humidity;
       currentHum.textContent = "Humidity" + "   " + data.main.humidity;
 
-      // var storedcities = JSON.parse(localStorage.getItem("cityList"));
-      // storedcities.push(data);
-      // localStorage.setItem("cityList", JSON.stringify(storedcities));
-      // if (storedcities != null) {
-      //   for (var x = 0; x < storedcities.length; x++) {
-      //     displaystoredcities.innerHTML +=
-      //       `<button class= city-button id= city-button href="#" value = "${storedcities[x]}" onclick = "previousSearch(this.value)">` +
-      //       storedcities[x] +
-      //       `</button>`;
-      //   }
-      // }
+
 
       const lat = data.coord.lat;
       const lon = data.coord.lon;
@@ -55,33 +43,41 @@ const searchWeather = () => {
     });
 };
 
-// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
 const searchUV = (lat, lon) => {
-  var UVquery = "https://api.openweathermap.org/data/2.5/uvi?appid="+aPIKey+"&lat="+lat+"&lon="+lon;
+  var UVquery =
+    "https://api.openweathermap.org/data/2.5/uvi?appid=" +
+    aPIKey +
+    "&lat=" +
+    lat +
+    "&lon=" +
+    lon;
   $.ajax({
     url: UVquery,
     method: "get",
   }).then((dataA) => {
     console.log(dataA);
-    currentUV.textContent = "UV Index " + dataA.value
+    currentUV.textContent = "UV Index " + dataA.value;
   });
-
- 
 };
 
 const weekView = (lat, lon) => {
-  var weekquery = "https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid="+aPIKey + "&units=imperial";
+  var weekquery =
+    "https://api.openweathermap.org/data/2.5/forecast?lat=" +
+    lat +
+    "&lon=" +
+    lon +
+    "&appid=" +
+    aPIKey +
+    "&units=imperial";
   $.ajax({
     url: weekquery,
     method: "get",
   }).then((datab) => {
     console.log(datab);
-    for (x=0; x<5; x++)
-    {
-      weeklytemp.textContent = (datab.list[0])
+    for (x = 0; x < 5; x++) {
+      weeklytemp.textContent = datab.list[1].main.temp;
     }
-  })
-}
+  });
+};
 
 button.addEventListener("click", displayWeather);
